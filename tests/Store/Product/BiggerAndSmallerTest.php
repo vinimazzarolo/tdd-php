da0808edd5e467203f0eb281ae7b29e74b79420e
Vinícius Mazzarolo <vinicius.mazzarolo@bling.com.br>
2023-03-15 19:20:12 -0300

    test: implement bigger and smaller test

---
+++ tests/Store/Product/BiggerAndSmallerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Store\Product;
+
+use App\Store\Cart\Cart;
+use App\Store\Produtct\BiggerAndSmaller;
+use App\Store\Produtct\Product;
+use PHPUnit\Framework\TestCase;
+
+class BiggerAndSmallerTest extends TestCase
+{
+    public function testAscOrder() {
+        $cart = new Cart();
+        $cart->add(new Product('Geladeira', 450.0));
+        $cart->add(new Product('Liquidificador', 250.0));
+        $cart->add(new Product('Jogo de pratos', 70.0));
+
+        $biggerAndSmaller = new BiggerAndSmaller();
+        $biggerAndSmaller->find($cart);
+
+        $this->assertEquals('Jogo de pratos', $biggerAndSmaller->getSmaller()->getName());
+        $this->assertEquals('Geladeira', $biggerAndSmaller->getBigger()->getName());
+    }
+}
