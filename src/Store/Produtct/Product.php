96e14d350943a759dbb8bbacd299d28062407f9a
Vinícius Mazzarolo <vinicius.mazzarolo@bling.com.br>
2023-03-16 21:09:23 -0300

    feat: add price to Product

--- src/Store/Produtct/Product.php
+++ src/Store/Produtct/Product.php
@@ -5,20 +5,31 @@ namespace App\Store\Produtct;
 class Product
 {
     public function __construct(
-        public string $name,
-        public float $price
+        private string $name,
+        private float $price,
+        private int $quantity = 1
     )
     {
 
     }
 
-    public function getName()
+    public function getName(): string
     {
         return $this->name;
     }
 
-    public function getPrice()
+    public function getPrice(): float
     {
         return $this->price;
+    }
+
+    public function getQuantity(): int
+    {
+        return $this->quantity;
+    }
+
+    public function getTotalPrice(): float
+    {
+        return $this->price * $this->quantity;
     }
 }
