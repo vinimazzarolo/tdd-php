27e68782188b4f93a15dc36013b9261bc7f4d3c3
Vinícius Mazzarolo <vinicius.mazzarolo@bling.com.br>
2023-03-20 20:48:17 -0300

    refactor: implements phpunit setup method

--- tests/Store/Cart/CartTest.php
+++ tests/Store/Cart/CartTest.php
@@ -8,32 +8,34 @@ use PHPUnit\Framework\TestCase;
 
 class CartTest extends TestCase
 {
+    private Cart $cart;
+
+    protected function setUp(): void
+    {
+        $this->cart = new Cart();
+        parent::setUp();
+    }
+
     public function testShouldReturnZeroIfCartIsEmpty()
     {
-        $cart = new Cart();
-        $biggerPrice = $cart->biggerPrice();
-
+        $biggerPrice = $this->cart->biggerPrice();
         $this->assertEquals(0, $biggerPrice);
     }
 
     public function testShouldReturnProductPriceIfCartHas1Element()
     {
-        $cart = new Cart();
-        $cart->add(new Product('Geladeira', 900.0, 1));
-
-        $biggerPrice = $cart->biggerPrice();
-
+        $this->cart->add(new Product('Geladeira', 900.0, 1));
+        $biggerPrice = $this->cart->biggerPrice();
         $this->assertEquals(900, $biggerPrice);
     }
 
     public function testShouldReturnBiggerPriceIfCartHasMultipleElements()
     {
-        $cart = new Cart();
-        $cart->add(new Product('Geladeira', 900.0, 1));
-        $cart->add(new Product('Fogão', 1500.0, 1));
-        $cart->add(new Product('Máquina de lavar', 750.0, 1));
+        $this->cart->add(new Product('Geladeira', 900.0, 1));
+        $this->cart->add(new Product('Fogão', 1500.0, 1));
+        $this->cart->add(new Product('Máquina de lavar', 750.0, 1));
 
-        $biggerPrice = $cart->biggerPrice();
+        $biggerPrice = $this->cart->biggerPrice();
 
         $this->assertEquals(1500.0, $biggerPrice);
     }
